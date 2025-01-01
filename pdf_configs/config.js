509203fb215ddb4985aff34982990d9a6f61d987
daaaaahara <quietree.daaaaahara@gmail.com>
2025-01-01 14:48:45 +0900

    Github Markdown StyleでPDFを生成する

--- pdf_configs/config.js
+++ pdf_configs/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stylesheet: "./pdf-configs/style.css",
+  stylesheet: "./pdf_configs/style.css",
   body_class: "markdown-body",
   marked_options: {
     headerIds: false,
@@ -7,9 +7,9 @@ module.exports = {
   },
   pdf_options: {
     "format": "A4",
-    "margin": "30mm 20mm",
+    "margin": "15mm 20mm",
     "printBackground": true,
-    "headerTemplate": "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
+    "headerTemplate": "<section></section>",
     "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
   },
   stylesheet_encoding: "utf-8",
